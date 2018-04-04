6ce65edde495c3d7f9e7e18ff83c0b1ff2e4a7b6
Zarema Khalilova <zarema.khalilova@gmail.com>
2018-04-04 20:14:03 +0300

    Add module wrapper for js code

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-"use strict";
+(function(global, factory) {
+  'use strict'
 
-(function() {
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = global.document
+      ? factory(global)
+      : function(w) {
+        if (!w.document) {
+          throw new Error('safescript requires a window with a document')
+        }
+        return factory(w)
+      }
+  } else {
+    factory(global)
+  }
+})(typeof window !== 'undefined' ? window : this, function(window) {
+  'use strict'
+
   var replace = function(safescript) {
     var script;
     safescript.style.display = 'none';
     if (safescript.childElementCount) {
       if (console.error) {
-        console.error("safescript syntax error: No other nodes allowed inside. " +
-                      "Please, escape the content with HTML entities.");
+        console.error('safescript syntax error: No other nodes allowed inside. ' +
+          'Please, escape the content with HTML entities.');
       }
       return;
     }
-    script = document.createElement('script');
+    script = window.document.createElement('script');
     script.text = safescript.textContent;
     safescript.parentElement.replaceChild(script, safescript)
   };
   var run = function() {
-    var safescripts = [].slice.call(document.getElementsByTagName('safescript'));
+    var safescripts = [].slice.call(window.document.getElementsByTagName('safescript'));
     safescripts.forEach(replace);
   };
 
-  var state = document.readyState;
+  var state = window.document.readyState;
   if (state === 'complete' || state === 'interactive') {
     run();
   } else {
-    document.addEventListener("DOMContentLoaded", run);
+    window.document.addEventListener('DOMContentLoaded', run);
   };
-})();
+})
